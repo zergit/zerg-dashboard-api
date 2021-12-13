b4090fdd287879c1f2b219fd16a23f8c5b6cc6ea
Sergey Igrunin <igrunen@bk.ru>
2021-12-13 16:38:03 +0300

    logger i controller

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,32 @@
 import express, { Express} from 'express';
-import { userRouter } from './users/users';
 import {Server} from 'http';
+import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
 
 export class App {
 	app: Express;
 	server: Server;
 	port: number;
+	logger: LoggerService;
+	userController: UserController;
 
-	constructor() {
+	constructor(
+		logger: LoggerService,
+		userController: UserController
+		) {
 		this.app = express();
 		this.port = 8000;
+		this.logger = logger;
+		this.userController = userController;
 }
 
 useRoutes() {
-	this.app.use('./users', userRouter);
+	this.app.use('./users', this.userController.router);
 }
 
 public async init() {
 	this.useRoutes();
 	this.server = this.app.listen(this.port);
-	console.log(`Сервер запущен на http://localhost:${this.port}`);
+	this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
 }
 }
